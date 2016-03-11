b8a99bd236605c8c7edf81fa4453afb4abb219cd
Robert <rwgeaston@gmail.com>
2016-03-11 12:38:55 +0000

    add DLC vehicles

--- vehicle_data.py
+++ vehicle_data.py
@@ -3,7 +3,9 @@ spec_order = {'speed': 0, 'acceleration': 1, 'weight': 2, 'handling': 3, 'grip':
 character_classes = {
     'babyweight': [2.25, 3.25, 2.25, 4.75, 4.5],
     'featherweight': [2.75, 3.00, 2.75, 4.25, 4.25],
+    'sub-lightweight': [3.25, 3, 3, 3.75, 4.00],
     'lightweight': [3.25, 2.75, 3.25, 3.75, 4.00],
+    'sub-midweight': [3.75, 2.75, 3.5, 3.25, 3.75],
     'midweight': [3.75, 2.50, 3.75, 3.25, 3.75],
     'cruiserweight': [4.25, 2.25, 4.25, 2.75, 3.50],
     'metalweight': [4.25, 2.00, 4.75, 2.75, 3.25],
@@ -24,10 +26,15 @@ characters = {
     'Toadette': 'featherweight',
     'Larry': 'featherweight',
     'Wendy': 'featherweight',
+    'Isabelle': 'featherweight',
+    'Cat Peach': 'sub-lightweight',
+    'Villager (female)': 'sub-lightweight',
     'Peach': 'lightweight',
     'Daisy': 'lightweight',
     'Yoshi': 'lightweight',
     'Mario': 'midweight',
+    'Tanooki Mario': 'sub-midweight',
+    'Villager (male)': 'sub-midweight',
     'Luigi': 'midweight',
     'Ludwig': 'midweight',
     'Iggy': 'midweight',
@@ -35,11 +42,13 @@ characters = {
     'Waluigi': 'cruiserweight',
     'Rosalina': 'cruiserweight',
     'Roy': 'cruiserweight',
+    'Link': 'cruiserweight',
     'Metal Mario': 'metalweight',
     'Pink Gold Peach': 'metalweight',
     'Bowser': 'heavyweight',
     'Wario': 'heavyweight',
     'Morton': 'heavyweight',
+    'Dry Bowser': 'heavyweight',
 }
 
 vehicles = {
@@ -72,6 +81,14 @@ vehicles = {
     'Mercedes GLA': [+0.00, -0.50, +0.50, -0.50, +0.50],
     'Mercedes W 25 Silver Arrow': [+0.00, +0.25, -0.25, +0.50, -0.50],
     'Mercedes 300 SL Roadster': [+0.00, +0.00, +0.00, +0.00, +0.00],
+    'Blue Falcon': [+0.25, +0.25, -0.25, 0, -0.5],
+    'Tanooki Kart': [0, -0.25, +0.25, -0.25, +0.25],
+    'B Dasher': [+0.5, -0.25, +0.25, 0, -1],
+    'Master Cycle': [0.25, 0, 0, 0.5, -0.75],
+    'Streetle': [+0.25, +0.25, -0.25, 0, -0.5],
+    'P-Wing': [+0.5, -0.25, +0.25, 0, -1],
+    'City Tripper': [+0.00, +0.25, -0.25, +0.50, -0.50],
+    'Bone Rattler': [+0.00, -0.50, +0.50, -0.50, +0.50],
 }
 
 vehicle_classes = {
@@ -104,6 +121,14 @@ vehicle_classes = {
     'Mercedes GLA': 'Kart',
     'Mercedes W 25 Silver Arrow': 'Kart', 
     'Mercedes 300 SL Roadster': 'Kart',
+    'Blue Falcon': 'Kart',
+    'Tanooki Kart': 'Kart',
+    'B Dasher': 'Kart',
+    'Master Cycle': 'Bike (in)',
+    'Streetle': 'Kart',
+    'P-Wing': 'Kart',
+    'City Tripper': 'Bike',
+    'Bone Rattler': 'Quad',
 }
 
 tyres = {
@@ -124,8 +149,10 @@ tyres = {
     'Sponge': [-0.25, +0.25, -0.25, -0.25, +0.50],
     'Cushion': [-0.25, +0.25, -0.25, -0.25, +0.50],
     'Metal': [+0.25, -0.50, +0.50, +0.00, -0.50],
-#    'Gold Tyres': [+0.25, -0.50, +0.50, +0.00, -0.50],
+    'Gold Tyres': [+0.25, -0.50, +0.50, +0.00, -0.50],
     'GLA Tyres': [+0.00, +0.00, +0.00, +0.00, +0.00],
+    'TriForce Tyres': [0.25, -0.25, 0, 0.25, -0.5],
+    'Leaf Tyres': [-0.50, +1.00, -0.50, +0.25, -0.25],
 }
 
 gliders = {
@@ -140,5 +167,7 @@ gliders = {
     'MKTV Parafoil': [+0.00, +0.25, -0.25, +0.00, +0.00],
     'Peach Parasol': [+0.00, +0.25, -0.25, +0.00, +0.00],
     'Parachute': [+0.00, +0.25, -0.25, +0.00, +0.00],
-    'Gold Glider': [+0.00, +0.00, +0.00, +0.00, +0.00]
+    'Gold Glider': [+0.00, +0.00, +0.00, +0.00, +0.00],
+    'Hylian Kite': [0, 0.25, -0.25, 0, 0],
+    'Paper Glider': [+0.00, +0.00, +0.00, +0.00, +0.00],
 }
