3657918e456e90ed507e396e4a4b9e7e9f3b1b15
Robert Easton <rwgeaston@gmail.com>
2016-03-16 14:57:03 +0000

    Update share_price.py

--- share_price.py
+++ share_price.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from yahoo_finance import Share
 
-csco = Share('CSCO')
+csco = None
 
 def yesterdays_price_from_timestamp(timestamp):
     # there are things like bank holidays and weekends
@@ -11,6 +11,12 @@ def yesterdays_price_from_timestamp(timestamp):
     # it might only return me one day
     # also any exception in this function needs to be non-fatal
     # can't risk breaking real functionality
+    global csco
+    if not csco:
+        try:
+            csco = Share('CSCO')
+        except:
+            return "Unknown"
     try:
         end = datetime.fromtimestamp(timestamp - 24 * 3600).strftime("%Y-%m-%d")
         start = datetime.fromtimestamp(timestamp - 5 * 24 * 3600).strftime("%Y-%m-%d")
